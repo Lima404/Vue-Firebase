237de31e20f5a8b1473ebc871c860dc27b3b8665
Lima404 <gabriel.lima2307@uol.com.br>
2024-02-04 15:38:28 -0300

    update with erros

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,32 +1,66 @@
 <template>
-  <div class="d-flex justify-content-center">
-    <div class="col-6 mt-2">
-      <b-card title="Card title">
-        <b-card-text>
-          Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
-          content.
-        </b-card-text>
+  <div>
+    <div class="d-flex justify-content-center" v-for="task in tasks" :key="task.id">
+      <div class="col-6 mt-2">
+        <b-card :title="task.subject">
+          <b-card-text>
+            {{ task.description }}
+          </b-card-text>
     
-        <div id="but">
-          <b-button variant="info" class="ml-2">Editar</b-button>
-          <b-button variant="danger" class="ml-2">Excluir</b-button>
-        </div>
-      </b-card>
+          <div id="but">
+            <b-button variant="info" class="ml-2" @click="updateTask(task.id)">Editar</b-button>
+            <b-button variant="danger" class="ml-2">Excluir</b-button>
+          </div>
+        </b-card>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
+  
   #but{
     display: flex;
     gap: 10px;
   }
   
-</style>
-
+  </style>
 <script>
+  // getDoc doc
+  import { db } from '../firebaseDb'
+  import {collection, onSnapshot} from "firebase/firestore"
+  //import Vue from 'vue'
   export default {
-    name: "ListView"
+    name: "ListView",
+    data(){
+      return{
+        tasks: []
+      }
+    },
+    created(){
+      this.getTasks();
+    },
+    methods: {
+      async getTasks(){
+        try{
+            onSnapshot(collection(db, "tasks"), (snapshot => {
+            this.tasks = [];
+
+            snapshot.forEach((doc) => {
+              let objectTask = {};
+              objectTask = doc.data();
+              objectTask.id = doc.id;
+              this.tasks.push(objectTask);
+            })
+          }),
+          )}
+          catch(e){
+            console.log('erro')
+        }
+      },
+      updateTask(idTask){
+        this.$router.push({ name: 'form', params: { id: idTask }})
+      }
+    }
   }
 </script>
